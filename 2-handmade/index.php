b9b01f3c04fd2bc804751edbbf2f69d85918a869
olivera <omicic@gmail.com>
2021-03-13 20:56:58 +0100

    header

---
+++ 2-handmade/index.php
@@ -0,0 +1,4 @@
+<?php include "partials/header.php"?>
+<h1>welcome</h1>
+<?php include "partials/footer.php"?>
+    
---
+++ 2-handmade/partials/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Handmade</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+
+    <header class="mainHeader">
+        <h1>Handmade</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php"></a>Home</li>
+                <li><a href="index.php"></a>About</li>
+                <li><a href="index.php"></a>Prodacts</li>
+                <li><a href="index.php"></a>Shop</li>
+                <li><a href="index.php"></a>Contact</li>
+            </ul>
+        </nav>
+
+    </header>
