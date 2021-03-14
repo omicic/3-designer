25149492159eed0b7cd618893bca793f6af92ba3
olivera <omicic@gmail.com>
2021-03-14 17:24:43 +0100

    nav fixed header

---
+++ 3-designer/partials/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Designer</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <header class="mainHeader">
+        <nav>
+            <ul>
+                <li><a href="index.php"></a>home</li>
+                <li><a href="index.php"></a>about</li>
+                <li><a href="index.php"></a>portfolio</li>
+                <li><a href="index.php"></a>blog</li>
+                <li><a href="index.php"></a>contact</li>
+            </ul>
+        </nav>
+
+        <div class="container">
+            <h1>Billy Joe Millano</h1>
+            <h3>web & graphic designer</h3>
+            <img src="img/designer.jpg" alt="">
+        </div>
+    </header>
