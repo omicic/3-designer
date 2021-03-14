be9284d5c921efd904367daffb8493537f99d536
olivera <omicic@gmail.com>
2021-03-14 17:56:19 +0100

    about, portfolio

--- 3-designer/index.php
+++ 3-designer/index.php
@@ -1,5 +1,61 @@
  <?php include('partials/header.php')?>
 
+ <section class="about">
+     <article class="container">
+         <div>
+             <h2>Graphic design</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+         <div>
+             <h2>Web design</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+         <div>
+             <h2>Technical SEO</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+     </article>
+     <hr>
+ </section>
 
+ <section class="portfolio">
+     <article class="container">
+         <div>
+             <img src="img/works1.jpg" alt="">
+             <h2>Graphic design</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+         <div>
+             <img src="img/works2.jpg" alt="">
+             <h2>Web design</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+         <div>
+             <img src="img/works3.jpg" alt="">
+             <h2>Technical SEO</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+         <div>
+             <img src="img/works4.jpg" alt="">
+             <h2>Technical SEO</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, veniam qui. Alias assumenda
+                 doloribus laudantium laboriosam quibusdam numquam qui temporibus.</p>
+             <button>read more</button>
+         </div>
+     </article>
+     <hr>
+ </section>
 
  <?php include('partials/footer.php')?>
